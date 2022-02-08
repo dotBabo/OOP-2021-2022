1dd24bc2757575320fdc950fbab4d44f16d9de52
Emmanuel <ebabarinde123@gmail.com>
2022-02-08 21:01:23 +0000

    rectangles added

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -4,6 +4,8 @@ import processing.core.PApplet;
 
 public class Loops extends PApplet
 {
+	float rectX, rectY;
+	//float rectWidth, rectHeight;
 
 	public void settings()
 	{
@@ -15,8 +17,17 @@ public class Loops extends PApplet
 		
 	}
 
+	void drawRect(float x, float y){
+		
+		for(int i = 0; i < 500; i+=50){
+		
+		rect(i,rectY,width / 10 ,height);
+		}
+	}
 	
 	public void draw()
 	{	
+		background(0);
+		drawRect(rectX, rectY);
 	}
 }
